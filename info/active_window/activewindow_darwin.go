1ac6373f85bb50a280c751c178a9a5bec5567bb9
Yuvraj Verma <vermayuvraj2010@gmail.com>
2026-02-07 22:06:51 +0530

    Swap from CGO to purego on MacOS

--- info/active_window/activewindow_darwin.go
+++ info/active_window/activewindow_darwin.go
@@ -2,21 +2,140 @@
 
 package active_window
 
-/*
-#cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework AppKit
-#include <stdlib.h>
+import (
+	"sync"
+	"unsafe"
 
-const char* active_app_name(void);
-*/
-import "C"
-import "unsafe"
+	"github.com/ebitengine/purego"
+)
+
+var (
+	CGWindowListCopyWindowInfo func(uint32, uint32) uintptr
+
+	CFArrayGetCount        func(uintptr) int64
+	CFArrayGetValueAtIndex func(uintptr, int64) unsafe.Pointer
+
+	CFDictionaryGetValue func(uintptr, unsafe.Pointer) unsafe.Pointer
+
+	CFNumberGetValue   func(uintptr, int32, unsafe.Pointer) bool
+	CFStringGetCString func(uintptr, unsafe.Pointer, int64, uint32) bool
+
+	CFRelease func(uintptr)
+
+	kCGWindowLayer     unsafe.Pointer
+	kCGWindowOwnerName unsafe.Pointer
+)
+
+var initOnce sync.Once
+
+func initFunctions() {
+	coreGraphics, err := purego.Dlopen(
+		"/System/Library/Frameworks/CoreGraphics.framework/CoreGraphics",
+		purego.RTLD_NOW|purego.RTLD_GLOBAL,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	coreFoundation, err := purego.Dlopen(
+		"/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation",
+		purego.RTLD_NOW|purego.RTLD_GLOBAL,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	purego.RegisterLibFunc(&CGWindowListCopyWindowInfo, coreGraphics, "CGWindowListCopyWindowInfo")
+
+	purego.RegisterLibFunc(&CFArrayGetCount, coreFoundation, "CFArrayGetCount")
+	purego.RegisterLibFunc(&CFArrayGetValueAtIndex, coreFoundation, "CFArrayGetValueAtIndex")
+	purego.RegisterLibFunc(&CFDictionaryGetValue, coreFoundation, "CFDictionaryGetValue")
+	purego.RegisterLibFunc(&CFNumberGetValue, coreFoundation, "CFNumberGetValue")
+	purego.RegisterLibFunc(&CFStringGetCString, coreFoundation, "CFStringGetCString")
+	purego.RegisterLibFunc(&CFRelease, coreFoundation, "CFRelease")
+
+	sym, err := purego.Dlsym(coreGraphics, "kCGWindowLayer")
+	if err != nil {
+		panic(err)
+	}
+	kCGWindowLayer = *(*unsafe.Pointer)(unsafe.Pointer(sym))
+
+	sym, err = purego.Dlsym(coreGraphics, "kCGWindowOwnerName")
+	if err != nil {
+		panic(err)
+	}
+	kCGWindowOwnerName = *(*unsafe.Pointer)(unsafe.Pointer(sym))
+}
 
 func FetchActiveWindowName() string {
-	name := C.active_app_name()
-	if name == nil {
+	initOnce.Do(initFunctions)
+
+	windows := CGWindowListCopyWindowInfo(
+		kCGWindowListOptionOnScreenOnly|
+			kCGWindowListExcludeDesktopElements,
+		kCGNullWindowID,
+	)
+	if windows == 0 {
 		return ""
 	}
-	defer C.free(unsafe.Pointer(name))
-	return C.GoString(name)
+	defer CFRelease(windows)
+
+	count := CFArrayGetCount(windows)
+
+	for i := int64(0); i < count; i++ {
+		win := uintptr(
+			CFArrayGetValueAtIndex(windows, i),
+		)
+
+		// Layer check
+		layerNum := uintptr(
+			CFDictionaryGetValue(win, kCGWindowLayer),
+		)
+		if layerNum == 0 {
+			continue
+		}
+
+		var layer int32
+		if !CFNumberGetValue(layerNum, kCFNumberIntType, unsafe.Pointer(&layer)) {
+			continue
+		}
+		if layer != 0 {
+			continue
+		}
+
+		// Owner name
+		owner := uintptr(
+			CFDictionaryGetValue(win, kCGWindowOwnerName),
+		)
+		if owner == 0 {
+			continue
+		}
+
+		buf := make([]byte, 256)
+		if CFStringGetCString(
+			owner,
+			unsafe.Pointer(&buf[0]),
+			int64(len(buf)),
+			kCFStringEncodingUTF8,
+		) {
+			return string(buf[:clen(buf)])
+		}
+	}
+
+	return ""
 }
+
+func clen(b []byte) int {
+	for i, c := range b {
+		if c == 0 {
+			return i
+		}
+	}
+	return len(b)
+}
+
+const kCGWindowListOptionOnScreenOnly = 1 << 0
+const kCGWindowListExcludeDesktopElements = 1 << 4
+const kCGNullWindowID = 0
+const kCFNumberIntType = 9
+const kCFStringEncodingUTF8 = 0x08000100
--- info/volume/volume_darwin.go
+++ info/volume/volume_darwin.go
@@ -3,28 +3,121 @@
 package volume
 
 import (
-	"bytes"
-	"context"
 	"log"
-	"os/exec"
-	"strings"
+	"strconv"
+	"sync"
+	"unsafe"
+
+	"github.com/ebitengine/purego"
 )
 
-func FetchVolume() string {
-	cmd := exec.CommandContext(
-		context.Background(),
-		"osascript",
-		"-e", "output volume of (get volume settings)",
+var audioObjectGetPropertyData func(uint32, *AudioObjectPropertyAddress, uint32, unsafe.Pointer, *uint32, unsafe.Pointer) int32 = nil
+var audioObjectHasPropertyData func(uint32, *AudioObjectPropertyAddress) bool = nil
+
+var initOnce sync.Once
+
+func initFunctions() {
+	coreAudio, err := purego.Dlopen(
+		"/System/Library/Frameworks/CoreAudio.framework/CoreAudio",
+		purego.RTLD_NOW|purego.RTLD_GLOBAL,
 	)
 
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	if err != nil {
+		log.Fatalf("Unable to load core audio: %v", err)
+	}
 
-	if err := cmd.Run(); err != nil {
-		log.Printf("Unable to fetch volume: %v", err)
+	purego.RegisterLibFunc(&audioObjectGetPropertyData, coreAudio, "AudioObjectGetPropertyData")
+	purego.RegisterLibFunc(&audioObjectHasPropertyData, coreAudio, "AudioObjectHasProperty")
+	purego.RegisterLibFunc(&audioObjectGetPropertyData, coreAudio, "AudioObjectGetPropertyData")
+}
+
+func FetchVolume() string {
+	initOnce.Do(initFunctions)
+
+	var deviceID uint32
+	dataSize := uint32(unsafe.Sizeof(deviceID))
+
+	getOutputDeviceAddress := AudioObjectPropertyAddress{
+		Selector: kAudioHardwarePropertyDefaultOutputDevice,
+		Scope:    kAudioObjectPropertyScopeGlobal,
+		Element:  kAudioObjectPropertyElementMain,
+	}
+
+	status := audioObjectGetPropertyData(
+		kAudioObjectSystemObject,
+		&getOutputDeviceAddress,
+		0,
+		nil,
+		&dataSize,
+		unsafe.Pointer(&deviceID),
+	)
+
+	if status != kAudioHardwareNoError {
 		return ""
 	}
 
-	v := strings.TrimSpace(out.String())
-	return v, nil
+	getVolumeAddress := AudioObjectPropertyAddress{
+		Selector: kAudioHardwareServiceDeviceProperty_VirtualMainVolume,
+		Scope:    kAudioObjectPropertyScopeOutput,
+		Element:  kAudioObjectPropertyElementMain,
+	}
+
+	if !audioObjectHasPropertyData(deviceID, &getVolumeAddress) {
+		return ""
+	}
+
+	var volume float32
+	dataSize = uint32(unsafe.Sizeof(volume))
+
+	status = audioObjectGetPropertyData(
+		deviceID,
+		&getVolumeAddress,
+		0,
+		nil,
+		&dataSize,
+		unsafe.Pointer(&volume),
+	)
+
+	if status != kAudioHardwareNoError {
+		return ""
+	}
+
+	return strconv.Itoa(int(volume * 100))
 }
+
+/*
+ The following structs/consts were found in AudioHardwareBase.h
+	AudioObjectPropertyAddress      struct
+
+	kAudioObjectPropertyScopeGlobal const
+	kAudioObjectPropertyScopeOutput const
+	kAudioObjectPropertyElementMain const
+	kAudioHardwareNoError           const
+*/
+
+type AudioObjectPropertyAddress struct {
+	Selector uint32
+	Scope    uint32
+	Element  uint32
+}
+
+const kAudioObjectPropertyScopeGlobal uint32 = 0x676C6F62 // 'glob'
+const kAudioObjectPropertyScopeOutput uint32 = 0x6F757470 // 'outp'
+const kAudioObjectPropertyElementMain uint32 = 0
+const kAudioHardwareNoError int32 = 0
+
+/*
+ The following constants were found in AudioHardware.h
+	kAudioObjectSystemObject                  const
+	kAudioHardwarePropertyDefaultOutputDevice const
+*/
+
+const kAudioObjectSystemObject uint32 = 1
+const kAudioHardwarePropertyDefaultOutputDevice uint32 = 0x644F7574
+
+/*
+ The following constants were found in AudioHardwareService.h
+	kAudioHardwareServiceDeviceProperty_VirtualMainVolume const
+*/
+
+const kAudioHardwareServiceDeviceProperty_VirtualMainVolume uint32 = 0x766D7663
